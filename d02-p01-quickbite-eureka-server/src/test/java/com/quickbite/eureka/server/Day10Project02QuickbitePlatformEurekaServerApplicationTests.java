4f234834d4abc56e8bc5451a1f9633a154002982
Shakir_kdl <shaikhsakir194@gmail.com>
2026-02-14 18:03:15 +0530

    Add Day01 Eureka discovery user service

---
+++ d02-p01-quickbite-eureka-server/src/test/java/com/quickbite/eureka/server/Day10Project02QuickbitePlatformEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickbite.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day10Project02QuickbitePlatformEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ d03-p01-config-server/src/test/java/com/amazon/config/Day11Project01ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazon.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day11Project01ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
