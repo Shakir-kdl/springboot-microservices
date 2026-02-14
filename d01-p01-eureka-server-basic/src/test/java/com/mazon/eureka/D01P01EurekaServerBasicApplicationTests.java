b36d59a10f7f4466a58675ed4d1bb5d04d1cebc7
Shakir_kdl <shaikhsakir194@gmail.com>
2026-02-14 17:56:24 +0530

    Add Day01 Eureka Server (basic)

---
+++ d01-p01-eureka-server-basic/src/test/java/com/mazon/eureka/D01P01EurekaServerBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazon.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D01P01EurekaServerBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
